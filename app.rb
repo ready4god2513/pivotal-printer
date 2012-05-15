0090c863bce613c51cfcee737809d7bb7e233dfb
Brandon Hansen <brandonh@ibethel.org>
2012-05-15 14:42:16 -0700

    Hmmm

--- app.rb
+++ app.rb
@@ -10,44 +10,35 @@ class PivotalPrinter < Sinatra::Base
   get "/stories" do
     @stories = Array.new
     
-    fetch_projects.each do |project|
+    PivotalTracker::Project.all.each do |project|
       project.stories.all.each do |story|
         @stories.push({
           id: story.id,
           project_id: project.id,
-  				url: story.url,
-  				created_at: story.created_at,
-  				accepted_at: story.accepted_at,
-  				project: project.name,
-  				name: story.name,
-  				description: story.description,
-  				story_type: story.story_type,
-  				estimate: story.estimate,
-  				current_state: story.current_state,
-  				requested_by: story.requested_by,
-  				owned_by: story.owned_by,
-  				owned: story.owned_by.nil?,
-  				estimateable: story.story_type.include?("feature"),
-  				labels: story.labels,
-  				deadline: story.deadline,
-  				notes: story.notes,
-  				tasks: story.tasks
+    			url: story.url,
+    			created_at: story.created_at,
+    			accepted_at: story.accepted_at,
+    			project: project.name,
+    			name: story.name,
+    			description: story.description,
+    			story_type: story.story_type,
+    			estimate: story.estimate,
+    			current_state: story.current_state,
+    			requested_by: story.requested_by,
+    			owned_by: story.owned_by,
+    			owned: story.owned_by.nil?,
+    			estimateable: story.story_type.include?("feature"),
+    			labels: story.labels,
+    			deadline: story.deadline,
+    			notes: story.notes,
+    			tasks: story.tasks
         })
       end
     end
     
+    content_type "application/json"
     @stories.to_json
   end
   
-  get "/projects/:project_id/story/:id" do |project_id, id|
-    @project = PivotalTracker::Project.find(project_id)
-    @story = @project.stories.find(id)
-    
-    erb :show, :locals => {:project => @project, :story => @story}
-  end
-  
-  def fetch_projects
-    PivotalTracker::Project.all
-  end
   
 end
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,64 +1,78 @@
-$(function(){
-	
-	var Story = new Backbone.Model;
-	var stories = new Backbone.Collection;
-	
-	stories.comparator = function(story)
-	{
-	  return Story.get("name");
-	};
-	
-	stories.search = function(query)
-	{
-		return this.filter(function(story)
-		{
-			var found = false;
-			var filters = ["project", "name", "description", "type", "estimate", "state", "requester", "owner", "label", "note", "task"];
-			
-			_.each(filters, function(filter)
-			{
-				console.log(filter);
-			});
-			
-			if(story.get("name").toLowerCase().indexOf(query.toLowerCase()) != -1)
-			{
-				found = true;
-			}
+(function($){
 
-			return found;
-		});
-	};
-
-	stories.fetch({
+	window.Story = Backbone.Model.extend({
+		
+	});
+	
+	window.Stories = Backbone.Collection.extend({
+		model: Story,
 		url: "/stories",
-		success: function()
+		
+		search: function(query)
 		{
-			$("#waiting").fadeOut("normal", function(){
-				$(this).remove();
+			return this.filter(function(story)
+			{
+				var found = false;
+				var filters = ["project", "name", "description", "type", "estimate", "state", "requester", "owner", "label", "note", "task"];
+
+				_.each(filters, function(filter)
+				{
+
+				});
+
+				if(story.get("name").toLowerCase().indexOf(query.toLowerCase()) != -1)
+				{
+					found = true;
+				}
+
+				return found;
 			});
 		}
 	});
-
-	$("#search-form").on("submit", function(){
-		var display = $("#pivotal-projects ul.thumbnails");
-		var res = stories.search($("#search-form .search-query").val());
-		var result_count = $("#pivotal-projects #number-of-results");
-		display.html("Searching...");
-
-		if(res.length > 0)
+	
+	window.stories = new Stories();
+	window.stories.fetch();
+	
+	window.StoryView = Backbone.View.extend({
+		template: _.template($("#story-template").html()),
+		
+		render: function()
 		{
-			result_count.html(res.length + " stories found");
-			display.html("");
-			_.each(res, function(story){
-				display.append(ich.story(story.toJSON()));
-			});
+			$(this.el).html(this.template(this.model.toJSON()));	
+			return this;
 		}
-		else
-		{
-			display.html("");
-			result_count.html("No results found");
-		}
-
-		return false;
 	});
-});
+	
+	
+	$(function(){
+		
+		window.PivotalPrinter = Backbone.Router.extend({
+			routes: {
+				"" : "index",
+				"projects/search/:params": "search",
+				"projects/:project_id/story/:id": "print"
+			},
+
+			index: function()
+			{
+				
+			},
+
+			search: function()
+			{
+
+			},
+
+			print: function()
+			{
+
+			}
+		});
+		
+		
+		// Kick off the application
+        window.App = new PivotalPrinter();
+        Backbone.history.start({pushState: true});
+	});
+
+})(jQuery);
