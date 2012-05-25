a186a0318276bef03bd800b5d6ee6408cedce7a3
Brandon Hansen <brandonh@ibethel.org>
2012-05-25 15:39:00 -0700

    Rack environment

--- lib/config.rb
+++ lib/config.rb
@@ -1,5 +1,5 @@
 PivotalTracker::Client.token = "416389666dfa61bad7d773209ab49835"
 PivotalTracker::Client.use_ssl = true
 
-::RAILS_ENV = "development" # this is needed to work around a Jammit limitation
+::RAILS_ENV = ENV["RACK_ENV"] # this is needed to work around a Jammit limitation
 Jammit.load_configuration("#{File.expand_path(File.dirname(__FILE__))}/assets.yml")
